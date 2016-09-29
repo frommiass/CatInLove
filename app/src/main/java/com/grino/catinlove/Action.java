93a240463a9ef67721a94d02f2359949227007db
Grino <frommiass@yandex.ru>
2016-09-29 19:06:00 +0300

    Create Action class

--- app/src/main/java/com/grino/catinlove/Action.java
+++ app/src/main/java/com/grino/catinlove/Action.java
@@ -1,11 +1,35 @@
 package com.grino.catinlove;
 
+import android.content.Context;
+
 public class Action implements Nameable{
 
-    String name;
+    private Context ctx;
+    private String name;
+
+    private int experience;
+    private Resources resources;
+    private Condition condition;
+
+    public Action(Context ctx, String name, int experience, Resources resources, Condition condition) {
+        this.ctx = ctx;
+        this.name = name;
+        this.experience = experience;
+        this.resources = resources;
+        this.condition = condition;
+    }
+
+    public Action(Context ctx, String name){
+        this.ctx = ctx;
+        this.name = name;
+        this.experience = 0;
+        this.resources = new Resources(ctx);
+        this.condition = new Condition(ctx);
+    }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
+
 }
