c0707949fbd85c3ec0efbe1bb4c05ce171107d2f
Grino <frommiass@yandex.ru>
2016-09-29 17:05:31 +0300

    add indicators and ctx in Condition and String

--- app/src/main/java/com/grino/catinlove/Condition.java
+++ app/src/main/java/com/grino/catinlove/Condition.java
@@ -1,5 +1,21 @@
 package com.grino.catinlove;
 
+import android.content.Context;
+
 public class Condition {
 
+    Context ctx;
+
+    private Indicator satiety;
+    private Indicator mood;
+    private Indicator energy;
+
+    public Condition(Context ctx) {
+
+        this.ctx = ctx;
+
+        satiety = new Indicator(ctx.getString(R.string.indicator_satiety), 100);
+        mood = new Indicator(ctx.getString(R.string.indicator_mood), 100);
+        energy = new Indicator(ctx.getString(R.string.indicator_energy), 100);
+    }
 }
