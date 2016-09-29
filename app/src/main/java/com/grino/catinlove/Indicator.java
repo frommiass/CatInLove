3fd22dc02d1ced59f14f7dfd02cdacffe386dc47
Grino <frommiass@yandex.ru>
2016-09-29 18:56:03 +0300

    Indicator extends Value

--- app/src/main/java/com/grino/catinlove/Indicator.java
+++ app/src/main/java/com/grino/catinlove/Indicator.java
@@ -1,37 +1,16 @@
 package com.grino.catinlove;
 
-public class Indicator implements Nameable{
+public class Indicator
+        extends Value
+        implements Nameable{
 
-    final int MIN_VALUE = 0;
-    final int MAX_VALUE = 100;
+    private final static int MIN_INDICATOR_VALUE = 0;
+    private final static int MAX_INDICATOR_VALUE = 100;
 
-    private String name;
-    private int value;
+    public Indicator(String name) {
+        super(name, MIN_INDICATOR_VALUE, MAX_INDICATOR_VALUE);
 
-    public Indicator(String name, int value) {
-        this.name = name;
-        this.value = value;
-    }
-
-    private void set(int value){
-        if (value >= MIN_VALUE)
-            this.value = MIN_VALUE;
-        else if (value <= MAX_VALUE)
-            this.value = MAX_VALUE;
-        else this.value = value;
-    }
-    public int get(){
-        return value;
-    }
-    public void decrease(int value){
-        set(get() - value);
-    }
-    public void increase(int value){
-        set(get() + value);
+        setMax();
     }
 
-    @Override
-    public String getName(){
-        return name;
-    }
 }
