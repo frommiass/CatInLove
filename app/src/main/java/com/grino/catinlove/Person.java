8696104bbdc8b39e828b4c74761027a59ba2da49
Grino <frommiass@yandex.ru>
2016-09-29 18:07:52 +0300

    add level, experience and nameable in Person

--- app/src/main/java/com/grino/catinlove/Person.java
+++ app/src/main/java/com/grino/catinlove/Person.java
@@ -2,20 +2,34 @@ package com.grino.catinlove;
 
 import android.content.Context;
 
-public class Person {
+public class Person implements Nameable{
 
     private Context ctx;
 
     private String name;
     private Condition condition;
+    private int level;
+    private int experience;
 
     public Person(Context ctx, String name) {
         this.ctx = ctx;
         this.name = name;
         condition = new Condition(ctx);
+        this.level = 0;
+        this.experience = 0;
     }
 
+    @Override
     public String getName() {
         return name;
     }
+
+    public void levelUp(){
+        level++;
+    }
+
+    public void doTick(){
+
+    }
+
 }
