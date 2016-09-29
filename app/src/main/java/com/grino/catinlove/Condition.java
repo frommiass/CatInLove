98f57cc32595a0be6785221c7d9d7935d813bee5
Grino <frommiass@yandex.ru>
2016-09-29 17:20:35 +0300

    add doTick

--- app/src/main/java/com/grino/catinlove/Condition.java
+++ app/src/main/java/com/grino/catinlove/Condition.java
@@ -18,4 +18,11 @@ public class Condition {
         mood = new Indicator(ctx.getString(R.string.indicator_mood), 100);
         energy = new Indicator(ctx.getString(R.string.indicator_energy), 100);
     }
+
+    public void doTick(){
+        satiety.decrease(1);
+        mood.decrease(1);
+        energy.decrease(1);
+    }
+
 }
