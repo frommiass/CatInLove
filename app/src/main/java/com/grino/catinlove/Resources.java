ad1974027be80da3edf6ce9524761064a09e9d9e
Grino <frommiass@yandex.ru>
2016-10-10 15:34:14 +0300

    fix bug

--- app/src/main/java/com/grino/catinlove/Resources.java
+++ app/src/main/java/com/grino/catinlove/Resources.java
@@ -9,20 +9,20 @@ public class Resources
     public final static int KEY_REAL = 2000 + 2;
 
     public Resources(Context ctx) {
-        super(ctx);
-        put(KEY_FOOD, new Indicator(ctx.getString(R.string.indicator_energy)));
-        put(KEY_REAL, new Indicator(ctx.getString(R.string.indicator_mood)));
+        super();
+        put(KEY_FOOD, new Resource(ctx.getString(R.string.resource_food)));
+        put(KEY_REAL, new Resource(ctx.getString(R.string.resource_real)));
     }
 
     public Resources(Context ctx, int food, int real) {
-        super(ctx);
+        super();
         put(KEY_FOOD, new Resource(ctx.getString(R.string.resource_food), food));
-        put(KEY_FOOD, new Resource(ctx.getString(R.string.resource_food), real));
+        put(KEY_REAL, new Resource(ctx.getString(R.string.resource_real), real));
     }
 
     @Override
     public String toString() {
-        return "Resources: " + super.toString();
+        return "RES:" + super.toString();
     }
 
     public static boolean isResourcesKey(int key){
