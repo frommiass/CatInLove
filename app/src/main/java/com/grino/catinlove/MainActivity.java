4b77ff8ae89d912a86ee3b88aa879580546b4560
Grino <frommiass@yandex.ru>
2016-10-10 23:02:33 +0300

    fab invisible

--- app/src/main/java/com/grino/catinlove/MainActivity.java
+++ app/src/main/java/com/grino/catinlove/MainActivity.java
@@ -2,6 +2,7 @@ package com.grino.catinlove;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.pb_2) ProgressBar pbMoon;
     @BindView(R.id.pb_3) ProgressBar pbEnergy;
 
+    @BindView(R.id.fab) FloatingActionButton fab;
+
     private Player cat;
     private Context ctx;
 
@@ -46,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         //При первом запуске запросить имя у пользователя
         cat = new Player(ctx, "Влюбленный кот");
         updateIndicators(cat);
+
+        fab.setVisibility(View.INVISIBLE);
     }
 
     @OnClick(R.id.fab)
