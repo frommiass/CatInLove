02b513ec92f3ef9932b9212856dc45da96fc74d1
Grino <frommiass@yandex.ru>
2016-10-10 22:20:22 +0300

    delete dead import

--- app/src/main/java/com/grino/catinlove/MainActivity.java
+++ app/src/main/java/com/grino/catinlove/MainActivity.java
@@ -2,7 +2,6 @@ package com.grino.catinlove;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -10,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
