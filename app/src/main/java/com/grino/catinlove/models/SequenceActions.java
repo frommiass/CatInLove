4d4da1611a7a6fdfa18f525ad715e77e6b9b6585
Grino <frommiass@yandex.ru>
2016-10-15 16:23:40 +0300

    extends ArrayList

--- app/src/main/java/com/grino/catinlove/models/SequenceActions.java
+++ app/src/main/java/com/grino/catinlove/models/SequenceActions.java
@@ -4,24 +4,16 @@ import com.grino.catinlove.tools.MapSequences;
 
 import java.util.ArrayList;
 
-public class SequenceActions {
-
-    private ArrayList<Action> actions;
+public class SequenceActions
+        extends ArrayList<Action>{
 
     public SequenceActions(String[] names, int[] iconIDs, MapSequences map) {
         Action action;
         for (int i=0; i< names.length; i++) {
-            action = new Action(names[i], iconIDs[0], map.getLine(i));
-            actions.add(action);
+            action = new Action(map.getAction(i), names[i], iconIDs[0]);
+            add(action);
         }
     }
 
-    public Action get(int n){
-        return actions.get(n);
-    }
-
-    public int size(){
-        return actions.size();
-    }
 }
 
