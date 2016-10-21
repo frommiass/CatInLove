a69a05b277f227a23c793f3650b6d4fc0b829263
Grino <frommiass@yandex.ru>
2016-10-22 00:28:14 +0300

    add names and res id for eat and hunt

--- app/src/main/java/com/grino/catinlove/models/Actions.java
+++ app/src/main/java/com/grino/catinlove/models/Actions.java
@@ -11,6 +11,7 @@ import com.grino.catinlove.tools.MapSequences;
 import com.grino.catinlove.tools.Sequence;
 
 import java.util.EnumMap;
+import java.util.HashMap;
 
 public class Actions
         extends EnumMap<DO, SequenceActions> {
@@ -25,6 +26,101 @@ public class Actions
         Sequence seq1 = new Sequence(SEQUENCE_TYPE.NUMBERS);
         Sequence seq2 = new Sequence(SEQUENCE_TYPE.LINEAR_DEPENDENCE, 88, 4);
 
+        HashMap<DO, int[]> icons = new HashMap<>();
+
+        int[] relaxIcons = {
+                R.drawable.relax_01,
+                R.drawable.relax_02,
+                R.drawable.relax_03,
+                R.drawable.relax_04,
+                R.drawable.relax_05,
+                R.drawable.relax_06,
+                R.drawable.relax_07,
+                R.drawable.relax_08,
+                R.drawable.relax_09,
+                R.drawable.relax_10,
+                R.drawable.relax_11,
+                R.drawable.relax_12,
+                R.drawable.relax_13,
+                R.drawable.relax_14,
+                R.drawable.relax_15,
+                R.drawable.relax_16,
+                R.drawable.relax_17,
+                R.drawable.relax_18,
+                R.drawable.relax_19,
+                R.drawable.relax_20,
+                R.drawable.relax_21,
+                R.drawable.relax_22,
+                R.drawable.relax_23,
+                R.drawable.relax_24,
+                R.drawable.relax_25,
+                R.drawable.relax_26,
+                R.drawable.relax_27,
+                R.drawable.relax_28,
+                R.drawable.relax_29,
+                R.drawable.relax_30,
+                R.drawable.relax_31,
+                R.drawable.relax_32,
+                R.drawable.relax_33,
+                R.drawable.relax_34,
+                R.drawable.relax_35,
+        };
+
+        int[] eatIcons = {
+                R.drawable.eat_01,
+                R.drawable.eat_02,
+                R.drawable.eat_03,
+                R.drawable.eat_04,
+                R.drawable.eat_05,
+                R.drawable.eat_06,
+                R.drawable.eat_07,
+                R.drawable.eat_08,
+                R.drawable.eat_09,
+                R.drawable.eat_10,
+                R.drawable.eat_11,
+                R.drawable.eat_12,
+                R.drawable.eat_13,
+                R.drawable.eat_14,
+                R.drawable.eat_15,
+                R.drawable.eat_16,
+                R.drawable.eat_17,
+                R.drawable.eat_18,
+                R.drawable.eat_19
+        };
+
+        int[] huntIcons = {
+                R.drawable.hunt_01,
+                R.drawable.hunt_02,
+                R.drawable.hunt_03,
+                R.drawable.hunt_04,
+                R.drawable.hunt_05,
+                R.drawable.hunt_06,
+                R.drawable.hunt_07,
+                R.drawable.hunt_08,
+                R.drawable.hunt_09,
+                R.drawable.hunt_10,
+                R.drawable.hunt_11,
+                R.drawable.hunt_12,
+                R.drawable.hunt_13,
+                R.drawable.hunt_14,
+                R.drawable.hunt_15,
+                R.drawable.hunt_16,
+                R.drawable.hunt_17,
+                R.drawable.hunt_18,
+                R.drawable.hunt_19,
+                R.drawable.hunt_20,
+                R.drawable.hunt_21,
+                R.drawable.hunt_22,
+                R.drawable.hunt_23
+        };
+
+        icons.put(DO.RELAX, relaxIcons);
+        icons.put(DO.EAT, eatIcons);
+        icons.put(DO.PLAY, relaxIcons);
+        icons.put(DO.HUNT, huntIcons);
+        icons.put(DO.CREATE, relaxIcons);
+
+
         for (DO d: DO.values()){
             MapSequences s = new MapSequences(KEY.class);
             s.put(KEY.EXP, seq0);
@@ -35,45 +131,7 @@ public class Actions
             else if (d == DO.HUNT)   s.put(KEY.FOOD,    seq1);
             else if (d == DO.CREATE) s.put(KEY.FOOD,    seq1);
 
-            int[] icons = {
-                    R.drawable.relax_01,
-                    R.drawable.relax_02,
-                    R.drawable.relax_03,
-                    R.drawable.relax_04,
-                    R.drawable.relax_05,
-                    R.drawable.relax_06,
-                    R.drawable.relax_07,
-                    R.drawable.relax_08,
-                    R.drawable.relax_09,
-                    R.drawable.relax_10,
-                    R.drawable.relax_11,
-                    R.drawable.relax_12,
-                    R.drawable.relax_13,
-                    R.drawable.relax_14,
-                    R.drawable.relax_15,
-                    R.drawable.relax_16,
-                    R.drawable.relax_17,
-                    R.drawable.relax_18,
-                    R.drawable.relax_19,
-                    R.drawable.relax_20,
-                    R.drawable.relax_21,
-                    R.drawable.relax_22,
-                    R.drawable.relax_23,
-                    R.drawable.relax_24,
-                    R.drawable.relax_25,
-                    R.drawable.relax_26,
-                    R.drawable.relax_27,
-                    R.drawable.relax_28,
-                    R.drawable.relax_29,
-                    R.drawable.relax_30,
-                    R.drawable.relax_31,
-                    R.drawable.relax_32,
-                    R.drawable.relax_33,
-                    R.drawable.relax_34,
-                    R.drawable.relax_35,
-            };
-
-            put(d, icons, s);
+            put(d, icons.get(d), s);
         }
 
         return this;
