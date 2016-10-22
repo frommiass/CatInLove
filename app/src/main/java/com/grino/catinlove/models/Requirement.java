af557a0677ca3949138536ad8a37ca97797c3829
Grino <frommiass@yandex.ru>
2016-10-22 04:01:29 +0300

    add Requirement class

---
+++ app/src/main/java/com/grino/catinlove/models/Requirement.java
@@ -0,0 +1,16 @@
+package com.grino.catinlove.models;
+
+import com.grino.catinlove.tools.Sequence;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+public class Requirement {
+
+    private int level;
+
+}
