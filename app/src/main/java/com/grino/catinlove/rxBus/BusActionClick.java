b07876f594ab6b542eb13f8b6ff77c6edb076a95
Grino <frommiass@yandex.ru>
2016-10-29 22:24:25 +0300

    fix strings and rxBus

--- app/src/main/java/com/grino/catinlove/rxBus/BusActionClick.java
+++ app/src/main/java/com/grino/catinlove/rxBus/BusActionClick.java
@@ -3,8 +3,10 @@ package com.grino.catinlove.rxBus;
 import com.grino.catinlove.models.Action.Action;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class BusActionClick
         extends BusEvent{
     private final Action action;
