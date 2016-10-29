47ef79f099208e21b95e196bda3721f747f73456
Grino <frommiass@yandex.ru>
2016-10-30 01:52:39 +0300

    set eat icon

--- app/src/main/java/com/grino/catinlove/enums/DO.java
+++ app/src/main/java/com/grino/catinlove/enums/DO.java
@@ -13,8 +13,8 @@ public enum DO {
     EAT    (R.string.do_eat, 2000 + 2, R.array.do_eat, R.string.do_eat_ru, R.string.do_eat_activate, R.array.do_eat_activate, R.drawable.mustache),
     RELAX  (R.string.do_relax, 2000 + 3, R.array.do_relax, R.string.do_relax_ru, R.string.do_relax_activate, R.array.do_relax_activate, R.drawable.paws),
 
-    HUNT   (R.string.do_hunt, 2000 + 4, R.array.do_hunt, R.string.do_hunt_ru, R.string.do_hunt_activate, R.array.do_hunt_activate, R.drawable.placeholder),
-    CREATE (R.string.do_create, 2000 + 5, R.array.do_create, R.string.do_create_ru, R.string.do_create_activate, R.array.do_create_activate, R.drawable.placeholder),
+    HUNT   (R.string.do_hunt, 2000 + 4, R.array.do_hunt, R.string.do_hunt_ru, R.string.do_hunt_activate, R.array.do_hunt_activate, R.drawable.eat),
+    CREATE (R.string.do_create, 2000 + 5, R.array.do_create, R.string.do_create_ru, R.string.do_create_activate, R.array.do_create_activate, R.drawable.eat),
 
     ERROR  (R.string.do_error, 2000 + 0, R.array.do_error, R.string.do_error_ru, R.string.do_error_ru, R.array.do_error, R.drawable.placeholder);
 
